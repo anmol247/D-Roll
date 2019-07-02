27df958123c328da62dca577b81bb87c33801f7f
anmol247 <anmoltiwari11@gmail.com>
2019-07-02 21:38:43 +0530

    Initial Commit

---
+++ DRoll/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  DRoll
+//
+//  Created by Anmol Tiwari on 02/12/18.
+//  Copyright © 2018 Anmol Tiwari. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+@IBDesignable class MyButton: UIButton
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateCornerRadius()
+    }
+    
+    @IBInspectable var rounded: Bool = false {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    func updateCornerRadius() {
+        layer.cornerRadius = rounded ? frame.size.height / 2 : 0
+    }
+}
+
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateImage()
+    }
+
+    
+    @IBAction func rollPressed(_ sender: UIButton) {
+        updateImage()
+    }
+    
+    func updateImage(){
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+}
+
