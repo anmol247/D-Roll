f6c37a26caaaca45dc94ef6534c8b55883c22515
anmol247 <anmoltiwari11@gmail.com>
2019-07-03 23:42:14 +0530

    Final commit

--- DRoll/ViewController.swift
+++ DRoll/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 import UIKit
 
+//Round button function
 @IBDesignable class MyButton: UIButton
 {
     override func layoutSubviews() {
@@ -37,12 +38,12 @@ class ViewController: UIViewController {
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
+    //dice1 & dice2 ImageView
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         updateImage()
     }
 
@@ -51,6 +52,7 @@ class ViewController: UIViewController {
         updateImage()
     }
     
+    //Random number generator function
     func updateImage(){
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
